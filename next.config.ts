b97272564a2b99ce8c7a15d00aef72e467a5414b
yelnurk123 <yelnur@onesports.gg>
2026-05-11 15:11:51 +0200

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,1 @@
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
+const nextConfig = { typescript: { ignoreBuildErrors: true } }; export default nextConfig;
